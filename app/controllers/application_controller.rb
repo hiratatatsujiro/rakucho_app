45fffcd9b5f774141872a796eb8cd2e960e34109
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-07-08 05:35:24 +0900

    ユーザー詳細編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameter 
     devise_parameter_sanitizer.permit(:sign_up, 
                                            keys: [:nickname, :last_name, :first_name, :last_name_reading, :first_name_reading, :birthday, :introduction, :grade_id, :classroom_id, :number_id, :image])
+  
+    devise_parameter_sanitizer.permit(:account_update, 
+                                           keys: [:nickname, :last_name, :first_name, :last_name_reading, :first_name_reading, :birthday, :introduction, :grade_id, :classroom_id, :number_id, :image])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "diaries#index"
+  root to: "home#top"
   resources :diaries, only: :index
+  resources :users, only: [:show, :edit, :update]
 end
